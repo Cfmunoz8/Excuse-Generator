37596ce0285a45400bea94abefdf3ff2825846ed
Cfmunoz8 <cfmunoz8@gmail.com>
2022-08-27 00:43:43 +0000

    Excusas en HTML con Bootstrap

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ let quien = ["mi perro", "mi gato", "mi hijo", "mi mamá", "mi vecino"];
 let que = ["la comió", "se sentó en ella", "la botó","la aplastó","la rompió"];
 let cuando = ["ayer", "justo antes de comenzar la clase", "mientras almorzaba", "cuando estaba en misa", "por la noche, mientras dormía"]
 
-function excuseGenerator (){
-    return ((quien[Math.floor(Math.random() * quien.length)]) +" "+ (que[Math.floor(Math.random() * que.length)]) +" "+ (cuando[Math.floor(Math.random() * cuando.length)]) +".")
+function excuseGenerator (arregloQuien, arregloQue, arregloCuando) {
+    let aleatorioQuien = Math.floor(Math.random() * arregloQuien.length);
+    let aleatorioQue = Math.floor(Math.random() * arregloQue.length);
+    let aleatorioCuando = Math.floor(Math.random() * arregloCuando.length);
+
+    return quien[aleatorioQuien] + " " + que[aleatorioQue] + " " + cuando[aleatorioCuando] + "."
 }
 
-console.log (excuseGenerator())
+let randomExcuse = excuseGenerator(quien, que, cuando)
+console.log (randomExcuse)
+
+const parrafo = document.querySelector("#excuse");
+parrafo.innerHTML = randomExcuse
